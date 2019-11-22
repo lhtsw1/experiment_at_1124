db40206372c3ca9416b672a14669b4c0f812b344
lht <lhtsw1@sjtu.edu.cn>
2019-11-22 12:26:09 +0800

    11.22

--- build/catkin_generated/order_packages.py
+++ build/catkin_generated/order_packages.py
@@ -2,4 +2,4 @@
 source_root_dir = "/home/lht/robots/src"
 whitelisted_packages = "".split(';') if "" != "" else []
 blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/lht/robots/devel;/opt/ros/kinetic".split(';') if "/home/lht/robots/devel;/opt/ros/kinetic" != "" else []
+underlay_workspaces = "/opt/ros/kinetic".split(';') if "/opt/ros/kinetic" != "" else []
--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -7,4 +7,10 @@
 # new environment variables
 
 # modified environment variables
+export CMAKE_PREFIX_PATH="/home/lht/robots/devel:$CMAKE_PREFIX_PATH"
+export LD_LIBRARY_PATH="/home/lht/robots/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/lht/robots/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/lht/robots/build"
+export PYTHONPATH="/home/lht/robots/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/lht/robots/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/lht/robots/src:$ROS_PACKAGE_PATH"
